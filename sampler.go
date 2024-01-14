e5f0009f8ccb220302cb80f52b1038c554d14196
Taimoor Ahmad <taimoor@opslevel.com>
2024-01-13 23:58:04 -0500

    simplify sampler function

--- sampler.go
+++ sampler.go
@@ -2,44 +2,14 @@ package opslevel_common
 
 import (
 	"math/rand"
-	"sort"
-	"time"
 )
 
-func getSamples(start int, end int, count int) []int {
-	rand.New(rand.NewSource(time.Now().UnixNano()))
-	if end < start || (end-start) < count {
-		return nil
-	}
-	nums := make([]int, 0)
-	for len(nums) < count {
-		num := rand.Intn(end-start) + start
-		exist := false
-		for _, v := range nums {
-			if v == num {
-				exist = true
-				break
-			}
-		}
-		if !exist {
-			nums = append(nums, num)
-		}
-	}
-	sort.Ints(nums)
-	return nums
-}
-
 func GetSample[T any](sampleCount int, data []T) []T {
-	if sampleCount < 1 {
+	if sampleCount < 1 || sampleCount > len(data) {
 		return data
 	}
-	totalItems := len(data)
-	if sampleCount >= totalItems {
-		return data
-	}
-	output := make([]T, sampleCount)
-	for i, index := range getSamples(0, totalItems, sampleCount) {
-		output[i] = data[index]
-	}
-	return output
+	rand.Shuffle(len(data), func(i, j int) {
+		data[i], data[j] = data[j], data[i]
+	})
+	return data[:sampleCount]
 }
